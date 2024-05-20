7955029db27a9e0f366dad7e84d3dd79d948a018
Fang Huang <huang.fang@northeastern.edu>
2024-05-19 17:15:54 -0700

    upload employee.c file for Lab2

--- lab-assignment2/employee.c
+++ lab-assignment2/employee.c
@@ -1,7 +1,12 @@
-/*C Program to read and print the n employee details using structure and dynamic memory allocation*/
+/* Name: Fang Huang, Summer_2024
+ * Email: huang.fang@northeastern.edu
+ * C Program to read and print the n employee details using structure and dynamic memory allocation*/
+
 #include <stdio.h>
 #include <stdlib.h>
+
 /*structure to hold the details of an employee*/
+
 typedef struct employee
 {
     int empId;
@@ -10,25 +15,56 @@ typedef struct employee
     char Dept[10];
 
 }employee_t;
+
+
 /* Read the employee details using scanf*/
-void readData(int n, employee_t* s)
-{
-    // Add your code here
+void readData(int n, employee_t* s){
+
+   for (int i = 0; i < n; i++) {
+	printf("Employee %d:\n", i+1);
+    	printf("please enter your ID: ");
+	scanf("%d", &(s->empId));
+	printf("name: ");
+	scanf("%s", s->Name);
+	printf("designation: ");
+	scanf("%s", s->Designation);
+	printf("department: ");
+	scanf("%s", s->Dept);
+	printf("\n");
+	s++;
+    }
+   
 }
-/* Function to print the employee details*/
-void display(int n, employee_t * s)
+
+
+/* function to print the employee details*/
+void display(int n, employee_t* s)
 {
-    // Add your code here
+    // display the details of this employee
+    for (int i = 0; i < n; i++) {
+	printf("The details of the employee %d:\n", i+1);
+	printf("Employee ID: %d\n", s -> empId);
+	printf("Name: %s\n", s->Name);
+	printf("Designation: %s\n", s->Designation);
+	printf("Department: %s\n\n", s-> Dept);
+	s++;
+    }
     
 }
 
-/*----------Main Program--------*/
-int main()
 
-{
+/*----main function-----*/
+
+int main() {
     // Main Function to print the employee details
-    // Add your code here
-    
+
+    printf("Enter the  number of employees: ");
+    int n;
+    scanf("%d", &n);
+    employee_t* s = (employee_t*) malloc(n*sizeof(employee_t));
+    readData(n, s);
+    display(n, s);
+    return 0;
 
 }
 
