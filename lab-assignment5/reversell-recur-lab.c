9eda503ce58f2b67c45c895149cc45d114419560
Fang Huang <huang.fang@northeastern.edu>
2024-06-06 15:28:43 -0700

    upload lab assignment 5

--- lab-assignment5/reversell-recur-lab.c
+++ lab-assignment5/reversell-recur-lab.c
@@ -1,4 +1,6 @@
-//enter your name and email here
+//Name: Fang Huang
+//Email: huang.fang@northeastern.edu
+
 #include<stdio.h>
 #include<stdlib.h>
 typedef struct node
@@ -14,17 +16,39 @@ typedef struct list{
 /*-----creating the nodes----------*/
 node_t* newNode(int num)
 {
+	node_t* node = (node_t*)malloc(sizeof(node_t));
+	if (node==NULL){
+		printf("Failed to create the node");
+		exit(1);
+	}
+	node -> next = NULL;
+	node -> data = num;
+	return node;
  
 }
 /*---creating linked list----*/
 List* init_LL(){
+	List* linkedList = (List*)malloc(sizeof(List));
+	if (linkedList == NULL){
+		printf("Failed to creat linked list");
+		exit(1);
+	}
+	linkedList -> head = NULL;
+	return linkedList;
     
 }
 
 /*---Insert the nodes at the begining of the list---*/
 void insertFirst(List* l, int data){
-    
+    node_t* node = newNode(data);
 
+    if (l -> head == NULL) {
+	    l -> head = node;
+    }else {
+	    node -> next = l -> head;
+	    l -> head = node;
+    }
+  
 }
 
 /*----display the output--------*/
@@ -42,14 +66,16 @@ void display(List* l)
 
 /*-------reversing the linked list using recursion------*/
 void reverse(List* l, node_t* ptr)
-{
-    
-    
-    
-    
-//insert your code here
-    
-    
+{  
+	//insert your code here
+	if(ptr == NULL || ptr->next == NULL) {
+		l -> head = ptr;
+		return;
+	}
+
+	reverse(l, ptr -> next);
+	ptr->next->next = ptr;
+	ptr->next = NULL;
 }
 
 /*----Free the nodes-----*/
