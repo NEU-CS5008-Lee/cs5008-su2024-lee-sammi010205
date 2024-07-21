edac9f97c58516da142d2cab1899be76ebf674c5
Fang Huang <huang.fang@northeastern.edu>
2024-07-21 16:17:49 -0700

    upload hw11c

--- assignment11/hw11c.c
+++ assignment11/hw11c.c
@@ -1,5 +1,5 @@
-// name: <your name here>
-// email: <your email here>
+// name: Fang Huang
+// email: huang.fang@northeastern.edu
 
 #include <stdio.h>
 
@@ -8,6 +8,21 @@ int d[20];
 long long int dp(int n) {
 
     // Add your code here
+    if (n==0) return 1;
+    if (n==1) return 0;
+    if (n==2) return 3;
+    d[0] = 1;
+    d[1] = 0;
+    d[2] = 3;
+
+    for (int i = 3; i <= n; i++) {
+	    d[i] += 3 * d[i-2];
+
+	    for (int j = i-4; j >= 0; j -= 2) {
+		    d[i] += 2 * d[j];
+	    }
+    }
+    return d[n];
 
 }
 
