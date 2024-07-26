38a449fb86b2ef26f0b94657ffe14c3f96e8ae9a
Fang Huang <huang.fang@northeastern.edu>
2024-07-26 10:52:35 -0700

    upload lab12

--- lab-assignment12/philosopher-lab.c
+++ lab-assignment12/philosopher-lab.c
@@ -1,9 +1,16 @@
-//Enter your name here
-//Enter your email here
+//Fang Huang
+//huang.fang@northeastern.edu
+
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
-pthread_mutex_t chopstick[5];//lock variable
+
+pthread_mutex_t chopstick[5]; // lock variables for chopsticks
+pthread_mutex_t lock; // single lock to control access to critical section
+int eat_count[5] = {0}; // array to keep track of how many times each philosopher has eaten
+int total_meals = 0; // total number of meals eaten by all philosophers
+const int max_eat_count = 3; // maximum number of times each philosopher can eat
+const int num_philosophers = 5; // number of philosophers
 
 // Thread to be executed
 void *philosopher(void *x)
@@ -13,7 +20,47 @@ void *philosopher(void *x)
     int n=*a;
     
     /*-----Insert your code here----*/
+
+    while (1) {
+        // Philosopher is thinking
+        printf("Philosopher %d is thinking.\n", n);
+        sleep(1);
+
+        // Lock the critical section
+        pthread_mutex_lock(&lock);
+        
+        if (total_meals >= num_philosophers * max_eat_count) {
+            pthread_mutex_unlock(&lock);
+            break;
+        }
+
+        // Pick up chopsticks
+        pthread_mutex_lock(&chopstick[n]); // pick up left chopstick
+        pthread_mutex_lock(&chopstick[(n + 1) % 5]); // pick up right chopstick
+
+        // Unlock the critical section
+        pthread_mutex_unlock(&lock);
+
+        // Philosopher is eating
+        printf("Philosopher %d is eating.\n", n);
+        sleep(2);
+        
+        // Increment eat count
+        pthread_mutex_lock(&lock);
+        eat_count[n]++;
+        total_meals++;
+        pthread_mutex_unlock(&lock);
+        
+        // Put down chopsticks
+        pthread_mutex_unlock(&chopstick[n]); // put down left chopstick
+        pthread_mutex_unlock(&chopstick[(n + 1) % 5]); // put down right chopstick
+
+        // Philosopher is done eating
+        printf("Philosopher %d is done eating %d times.\n", n, eat_count[n]);
+        sleep(1);
+    }
     
+    return NULL;
     
 }
 
